a40d7edecd8f1ed9f3a20226a5ae869ffe5a8ff6
junke <junke@sunbingchen.cn>
2021-06-30 17:12:12 +0800

    优化

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+import './aos.min.js' // 滚动页面加载
+import './skitter.min.js' // 轮播特效
+import '../js/fullScreen.min.js'; // 全屏
+import '../js/sidebar.min.js'; // 带本地存储功能的显示/隐藏侧栏
+import '../js/bootstrap-components.min.js'; // bootstrap组件
+import '../js/masonry.min.js'; // 多个分页合并成瀑布流页面
